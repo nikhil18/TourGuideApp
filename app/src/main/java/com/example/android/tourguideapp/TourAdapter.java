f1858572a77adedf3a200f5e76bcb55fa004dd67
nikhil18 <nikhilgolechha1@gmail.com>
2017-09-06 14:16:00 +0530

    add butter knife and viewholder to TourAdaper

--- app/src/main/java/com/example/android/tourguideapp/TourAdapter.java
+++ app/src/main/java/com/example/android/tourguideapp/TourAdapter.java
@@ -11,32 +11,54 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class TourAdapter extends ArrayAdapter<TourItem> {
+
+    private Context context;
 
     public TourAdapter(Context context, ArrayList<TourItem> tourItemArrayList) {
         super(context, 0, tourItemArrayList);
+        this.context = context;
     }
 
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
         TourItem tourItem = getItem(position);
+        ViewHolder viewHolder;
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageview_lv);
-        TextView titleTextView = (TextView) convertView.findViewById(R.id.title_textview_lv);
-        TextView infoTextView = (TextView) convertView.findViewById(R.id.info_textview_lv);
-        TextView locTextView = (TextView) convertView.findViewById(R.id.loc_textview_lv);
-
-        imageView.setImageResource(tourItem.getImage());
-        titleTextView.setText(tourItem.getTitle());
-        infoTextView.setText(tourItem.getInformation());
-        locTextView.setText(tourItem.getLocation());
+        viewHolder.imageview_lv.setImageResource(tourItem.getImage());
+        viewHolder.title_textview_lv.setText(tourItem.getTitle());
+        viewHolder.info_textview_lv.setText(tourItem.getInformation());
+        viewHolder.loc_textview_lv.setText(tourItem.getLocation());
 
         return convertView;
     }
+
+    // somewhere else in your class definition
+    static class ViewHolder {
+        @BindView(R.id.title_textview_lv)
+        TextView title_textview_lv;
+        @BindView(R.id.info_textview_lv)
+        TextView loc_textview_lv;
+        @BindView(R.id.loc_textview_lv)
+        TextView info_textview_lv;
+        @BindView(R.id.imageview_lv)
+        ImageView imageview_lv;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
 }
